d421de5b1cf53f631c1c8107baf607492aabb387
zeyd111 <zeyd.suleymanli44@gmail.com>
2023-11-28 21:01:10 +0400

    add code

--- main.c
+++ main.c
@@ -3,6 +3,31 @@
 
 int main()
 {
-   printf("hello world");
+
+int n,t;
+
+int Arr[20];
+
+int pos;
+
+int max,min,floorx;
+
+scanf("%d %d",&n,&t);
+
+for(int i = 0;i < n;i++)
+{
+scanf("%d",&Arr[i]);
+
+}
+
+scanf("%d",&pos);
+
+min = Arr[0];
+
+max = Arr[n-1];
+
+floorx = Arr[pos-1];
+
+
     return 0;
 }
